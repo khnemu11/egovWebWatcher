6bd97cff6a85bd5257dc43dce13927f6589c19f9
Junhyeon <wnsgus313@gmail.com>
2022-09-19 20:39:58 +0900

    User 관련 코드 작성

---
+++ src/main/java/egovframework/example/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,89 @@
+package egovframework.example.user.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import egovframework.example.cmmn.CommonServiceImpl;
+import egovframework.example.login.service.LoginVO;
+import egovframework.example.user.service.UserService;
+import egovframework.example.user.service.UserVO;
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+
+@Service("userService")
+public class UserServiceImpl extends CommonServiceImpl implements UserService {
+	@Resource(name = "userMapper")
+	private UserMapper userDAO;
+
+	/** ID Generation */
+	@Resource(name = "egovIdGnrService")
+	private EgovIdGnrService egovIdGnrService;
+	
+	@Override
+	public long selectUserEmail(UserVO vo) throws Exception {
+		return userDAO.selectUserEmail(vo);
+	}
+
+	@Override
+	public long selectUserEmailUpdate(UserVO vo) throws Exception {
+		return userDAO.selectUserEmail(vo);
+	}
+
+	@Override
+	public long selectUserLoginId(LoginVO vo) throws Exception {
+		return userDAO.selectUserLoginId(vo);
+	}
+
+	@Override
+	public long selectUserLoginIdUpdate(LoginVO vo) throws Exception {
+		return userDAO.selectUserLoginIdUpdate(vo);
+	}
+
+	@Override
+	public long selectUserSeq(UserVO vo) throws Exception {
+		return userDAO.selectUserSeq(vo);
+	}
+
+	@Override
+	public int insertTuser(UserVO vo) throws Exception {
+		return userDAO.insertTuser(vo);
+	}
+
+	@Override
+	public int insertTarclogin(LoginVO vo) throws Exception {
+		return userDAO.insertTarclogin(vo);
+	}
+
+	@Override
+	public long selectUserListCount(UserVO vo) throws Exception {
+		return userDAO.selectUserListCount(vo);
+	}
+
+	@Override
+	public List<Map<String, String>> selectUserList(UserVO vo) throws Exception {
+		return userDAO.selectUserList(vo);
+	}
+
+	@Override
+	public int updateTuser(UserVO vo) throws Exception {
+		return userDAO.updateTuser(vo);
+	}
+
+	@Override
+	public int updateTarclogin(LoginVO vo) throws Exception {
+		return userDAO.updateTarclogin(vo);
+	}
+
+	@Override
+	public int deleteTuser(UserVO vo) throws Exception {
+		return userDAO.deleteTuser(vo);
+	}
+
+	@Override
+	public int deleteTarclogin(LoginVO vo) throws Exception {
+		return userDAO.deleteTarclogin(vo);
+	}
+}
